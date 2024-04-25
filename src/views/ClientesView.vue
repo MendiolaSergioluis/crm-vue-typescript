d44c9f846b6d4ce8d55af969807e7821d6f6d0db
Sergioluis Mendiola <mendiolasergioluis@gmail.com>
2024-04-25 10:43:13 -0400

    Nuevo componente Heading creado y utilizado para sustituir los h1

--- src/views/ClientesView.vue
+++ src/views/ClientesView.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import RouterLink from "@/components/UI/RouterLink.vue";
+import Heading from "@/components/UI/AppHeading.vue";
 </script>
 
 <template>
@@ -7,6 +8,6 @@ import RouterLink from "@/components/UI/RouterLink.vue";
     <div class="flex justify-end">
       <RouterLink to="agregar-cliente">Agregar Clientes</RouterLink>
     </div>
-    <h1 class="text-4xl font-extrabold text-slate-500">Clientes</h1>
+    <Heading>Listado de Clientes</Heading>
   </div>
 </template>
