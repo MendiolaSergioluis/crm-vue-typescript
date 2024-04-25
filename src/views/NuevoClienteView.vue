d293bcc7a5a355625cc3e7fc282828544da45588
Sergioluis Mendiola <mendiolasergioluis@gmail.com>
2024-04-25 13:46:58 -0400

    Agregado el primer input con algunos de los props disponibles de FormKit

--- src/views/NuevoClienteView.vue
+++ src/views/NuevoClienteView.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import RouterLink from '@/components/UI/RouterLink.vue'
 import Heading from '@/components/UI/AppHeading.vue'
-import {FormKit} from "@formkit/vue";
+import { FormKit } from '@formkit/vue'
 
 defineProps<{
   titulo: string
@@ -15,10 +15,17 @@ defineProps<{
     </div>
     <Heading>{{ titulo }}</Heading>
     <div class="mx-auto mt-10 bg-white shadow">
-      <FormKit
-        type="form"
-      >
-
+      <FormKit type="form">
+        <FormKit
+          type="text"
+          label="nombre"
+          placeholder="Nombre del Cliente"
+          prefix-icon="add"
+          help="Coloca el nombre del cliente que deseas registrar"
+          validation="required"
+          :validation-messages="{ required: 'El nombre del cliente es obligatorio' }"
+          validation-visibility="live"
+        />
       </FormKit>
     </div>
   </div>
