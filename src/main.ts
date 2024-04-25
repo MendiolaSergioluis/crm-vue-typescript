af3578e6340d1f39f7caf0e1abde345d57165212
Sergioluis Mendiola <mendiolasergioluis@gmail.com>
2024-04-25 13:31:42 -0400

    Agregado el paquete @formkit/vue e inicio de formulario en NuevoClienteView.vue

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,16 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+// Importa plugins
 import router from './router'
+import { plugin, defaultConfig } from '@formkit/vue'
 
+// Crea la Aplicación
 const app = createApp(App)
 
+// Plugins
 app.use(router)
+app.use(plugin, defaultConfig)
 
+// Corre el proyecto
 app.mount('#app')
--- src/views/NuevoClienteView.vue
+++ src/views/NuevoClienteView.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import RouterLink from '@/components/UI/RouterLink.vue'
 import Heading from '@/components/UI/AppHeading.vue'
+import {FormKit} from "@formkit/vue";
 
 defineProps<{
   titulo: string
@@ -13,5 +14,12 @@ defineProps<{
       <RouterLink to="inicio">Volver</RouterLink>
     </div>
     <Heading>{{ titulo }}</Heading>
+    <div class="mx-auto mt-10 bg-white shadow">
+      <FormKit
+        type="form"
+      >
+
+      </FormKit>
+    </div>
   </div>
 </template>
